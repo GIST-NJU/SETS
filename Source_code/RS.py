da49081b8c69fc43ed7a298cb13098bfa79b52a0
JanelieWang <1240117096@qq.com>
2025-07-12 21:36:37 +0800

    Update RS.py

--- Source_code/RS.py
+++ Source_code/RS.py
@@ -1,11 +1,15 @@
 import random
+import time
 
 
-# This function is reused from the official replication package of DeepGD:
+# This function is directly taken from the official reproduction package of DeepGD:
 # https://github.com/ZOE-CA/DeepGD
 
 # size: test budget
 # index: the indices of all the test inputs
 def rs(size, index):
+    s_time = time.time()
     hh = random.sample(index, size)
-    return hh
+    e_time = time.time()
+    execution_time = e_time - s_time
+    return hh, execution_time
